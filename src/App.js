5236151860c3c8c544acbe25d431e651252e0f15
FelipeDeMoraes19 <moorraes16@gmail.com>
2025-02-15 16:24:51 -0300

    API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-
 function App() {
   return (
     <div>
       <h1>Docker Monitor</h1>
-      <p>Bem-vindo ao sistema de monitoramento de contêineres Docker.</p>
+      <ContainerList />
     </div>
   );
 }
